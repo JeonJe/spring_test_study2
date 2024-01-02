727c65a5fb136681e95db5da950c90627ea748a5
green <whssodi@gmail.com>
2024-01-02 22:03:15 +0900

    feat : repository test

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`,`email`,`nickname`,`address`,`certification_code`,`status`,`last_login_at`)
+values (1, 'whssodi@gmail.com', 'whssodi', 'Seoul', 'aaaaaa-aaa-aa', 'ACTIVE', 0);
