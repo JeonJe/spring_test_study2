6f42c567de832a32874bbc1d4501ed83797a7d55
green <whssodi@gmail.com>
2024-01-17 08:32:41 +0900

    feat : post 및 2개 테스트 코드 추가

--- src/test/java/com/example/demo/controller/PostCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/PostCreateControllerTest.java
@@ -1,26 +1,55 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.PostCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.JsonPath;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/post-create-controller-test-data.sql", executionPhase =  Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 public class PostCreateControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    void 헬스_체크_응답이_200() throws Exception {
-        mockMvc.perform(get("/health_check.html"))
-                .andExpect(status().isOk());
+    void 게시물_생성() throws Exception {
+
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(1)
+                .content("hello")
+                .build();
+
+        mockMvc.perform(
+                        post("/api/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("hello"))
+                .andExpect(jsonPath("$.writer.id").value(1))
+                .andExpect(jsonPath("$.writer.email").value("whssodi@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("whssodi"));
     }
 }
---
+++ src/test/resources/sql/post-controller-test-data.sql
@@ -0,0 +1,6 @@
+insert into `users` (`id`,`email`,`nickname`,`address`,`certification_code`,`status`,`last_login_at`)
+values (1, 'whssodi@gmail.com', 'whssodi', 'Seoul', 'aaaaaa-aaa-aa', 'ACTIVE', 0);
+
+insert into `posts` (`id`, `content`, `created_at`,  `modified_at`, `user_id`)
+values (1, 'helloword', 1678530673958, 0, 1);
+
